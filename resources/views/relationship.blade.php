46d51d21e19d0ff1f05dd0b1543bf105c8de5ea7
Daniel Reyes <danielreyesveas@gmail.com>
2017-01-23 10:27:54 -0300

    método whereHas

--- resources/views/relationship.blade.php
+++ resources/views/relationship.blade.php
@@ -6,17 +6,9 @@
     <body>
         @foreach($categories as $category)
         	<p>
-        		{{ $category->name }}
-        		({{ $category->num_books }})
-        	</p>
-        	<ul>
-        	@foreach($category->books as $book)
-        		<li>
-        			<strong>{{ $book->title }}</strong>
-        			{{ $book->description }}
-        		</li>
-        	@endforeach
-        	</ul>
+                {{ $category->name }}
+        		{{ $category->num_books }}
+        	</p>    
         @endforeach
     </body>
 </html>
